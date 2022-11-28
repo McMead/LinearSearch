f113f39662f2ef625d4b234e9ba17894ff9318ce
Michael Mugadza <michael_mugadza@dialog.com.au>
2022-11-28 10:09:51 +1000

    Add project files.

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(linearSearch(new int[] { 10, 20, 30 }, 10));
+
+
+int linearSearch(int[] arr, int val)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		if (arr[i] == val)
+			return i;
+	}
+	return -1;
+}
